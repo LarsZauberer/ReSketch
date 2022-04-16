2562b6a4d240df0016e43905e269ca2ffdd95569
RobRob1101 <robin.steiner00@gmail.com>
2022-04-16 16:41:54 +0200

    tests + drawline function

--- paper rebuild/environment.py
+++ paper rebuild/environment.py
@@ -3,6 +3,60 @@ import random
 import numpy as np
 import math as ma
 
+#draws Line directly on bitmap to save convert
+def drawline(setpos, pos, canvas):
+    weight = 1
+    dx = pos[0] - setpos[0] 
+    dy = pos[1] - setpos[1] 
+    linePix = []
+
+    if dx == 0 and dy == 0:
+        return
+
+    if abs(dx) > abs(dy):
+        inc = int(ma.copysign(1,dx))
+        for i in range(0, dx+inc, inc):
+            linePix.append([setpos[0]+i, 0])
+        
+        step = dy/(abs(dx)+1)
+        sign = int(ma.copysign(1,dy))
+        move = 0
+        res = 0
+        for i in range((abs(dx)+1)):
+            res += step
+            if sign*res >= 0.5:
+                move += sign
+                res -= sign
+            linePix[i][1] = setpos[1]+move
+
+        for pix in linePix:
+            canvas[pix[1]][pix[0]-weight] = 1
+            canvas[pix[1]][pix[0]+weight] = 1
+            canvas[pix[1]][pix[0]] = 1
+    else:
+        inc = int(ma.copysign(1,dy))
+        for i in range(0, dy+inc, inc):
+            linePix.append([0, setpos[1]+i])
+        
+        step = dx/(abs(dy)+1)
+        sign = int(ma.copysign(1,dx))
+        move = 0
+        res = 0
+        for i in range((abs(dy)+1)):
+            res += step
+            if sign*res >= 0.5:
+                move += sign
+                res -= sign
+            linePix[i][0] = setpos[0]+move
+
+        for pix in linePix:
+            canvas[pix[1]][pix[0]+weight] = 1
+            canvas[pix[1]][pix[0]-weight] = 1
+            canvas[pix[1]][pix[0]] = 1
+    
+    return canvas
+
+
 
 class ShapeDraw(object):
     def __init__(self, sidelength, patchsize, referenceData):
@@ -25,30 +79,32 @@ class ShapeDraw(object):
         #initializes rest
         self.reference = referenceData[0]
         self.curRef = 0
-        self.agentPos
-        self.set_agentPos(random.randrange(0, self.s), random.randrange(0, self.s))
-        
+        self.agentPos = [0,0]
+        self.isDrawing = 0 # 0 = not Drawing, 1 = Drawing (not bool because NN)
+        self.set_agentPos([random.randrange(0, self.s), random.randrange(0, self.s)])
 
-        self.lastReward
-        self.reward
-        
-    def set_agentPos(self, x, y):
-        self.agentPos = (x, y)
+         
+    def set_agentPos(self, pos):
+        if self.isDrawing:
+            self.canvas = drawline(self.agentPos, pos, self.canvas)
+        self.agentPos = pos
         self.update_distmap()
-
+        self.update_patch()
+        self.update_colmap()
+        
     def update_distmap(self):
-        x0 = self.agentPos[1]
-        y0 = self.agentPos[2]
+        x0 = self.agentPos[0]
+        y0 = self.agentPos[1]
         for y in range(self.s):
             for x in range(self.s):
                 dist =  ma.sqrt((x-x0)**2 + (y-y0)**2)/self.s
                 self.distmap[y][x] = dist
 
-    def update_colmap(self, state):
+    def update_colmap(self):
         for y in range(self.s):
             for x in range(self.s):
-                self.colmap[y][x] = state
-
+                self.colmap[y][x] = self.isDrawing
+    
     def update_patch(self, reference):
         patchX = self.agentPos[0]-(self.p-1)/2
         patchY = self.agentPos[1]-(self.p-1)/2
@@ -57,6 +113,21 @@ class ShapeDraw(object):
                 self.ref_patch[y][x] = reference[patchY+y][patchX+x]
                 self.canvas_patch[y][x] = self.canvas[patchY+y][patchX+x]
 
+    
+    
+    def step(self, agent_action):
+        action = [0,0]
+        self.isDrawing = 1
+        
+        x = agent_action % 11
+        y = agent_action // 11
+        if y >= 11:
+            y -= 11
+            self.isDrawing = 0
+        
+        action = [x,y]
+        self.set_agentPos(action)
+    
     def reset(self):
         self.curRef += 1
         self.reference = self.referenceData[self.curRef]
@@ -64,23 +135,35 @@ class ShapeDraw(object):
         self.update_colmap(0)
         self.set_agentPos(random.randrange(0, self.s), random.randrange(0, self.s))
 
-    
+        
 
 
-    
-
+        
+            
         
 
 
 
 
+env = ShapeDraw(84, 11, (5,5))
+
+print(len(env.canvas_patch))
+print(env.distmap)
 
 
-   
 
-arr = np.zeros(5, dtype=1)
 
-print(arr)
+    
+
+
+    
+
+
+    
+
+    
+
+    
     
 
 if __name__ == "__main__":
