1c441640c37661a88def592fd079fea3e815aebe
Robin <robin.steiner00@gmail.comm>
2022-04-16 20:30:28 +0200

    organization main program, agent, dqn

---
+++ paper rebuild/nn_agent.py
@@ -0,0 +1,18 @@
+class Agent():
+    def __init__(self, alpha, gamma, epsilon, n_actions):
+        self.alpha = alpha
+        self.gamma = gamma
+        self.epsilon = epsilon
+        self.actionSpace = [i for i in range(n_actions)]
+
+    def chooseAction(self):
+        x = 0
+    
+    def learn(self):
+        x = 0
+    
+    def save_model(self):
+        x = 0
+
+""" class DQN():
+    def __init__(): """
