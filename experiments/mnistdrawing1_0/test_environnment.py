d94741199c8f12c8dfaa75ca5d8a39d17b1375f2
LarsZauberer <wasser.ian@gmail.com>
2022-04-18 16:57:00 +0200

    Environnment working

--- experiments/mnistdrawing1_0/test_environnment.py
+++ experiments/mnistdrawing1_0/test_environnment.py
@@ -5,12 +5,25 @@ from experiments.mnistdrawing1_0.environnment import Canvas
 def test_calc_dir_length():
     env = Canvas()
     
-    assert env._calc_dir_length(1) == (1, 1)
-    assert env._calc_dir_length(5) == (1, 2)
+    assert env._calc_dir_length(1) == (1, 1, True)
+    assert env._calc_dir_length(5) == (1, 2, True)
     
-    assert env._calc_dir_length(4) == (4, 1)
-    assert env._calc_dir_length(8) == (4, 2)
+    assert env._calc_dir_length(4) == (4, 1, True)
+    assert env._calc_dir_length(8) == (4, 2, True)
     
     with pytest.raises(ValueError):
         env._calc_dir_length(0)
         env._calc_dir_length(201)
+
+def test_calc_dir_length_nodraw():
+    env = Canvas()
+    
+    assert env._calc_dir_length(-1) == (1, 1, False)
+    assert env._calc_dir_length(-5) == (1, 2, False)
+    
+    assert env._calc_dir_length(-4) == (4, 1, False)
+    assert env._calc_dir_length(-8) == (4, 2, False)
+    
+    with pytest.raises(ValueError):
+        env._calc_dir_length(0)
+        env._calc_dir_length(-201)
