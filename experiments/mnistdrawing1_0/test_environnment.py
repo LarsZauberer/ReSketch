e87de1018a2bc202b27225b25c6fd213f712edb8
LarsZauberer <wasser.ian@gmail.com>
2022-04-19 22:26:02 +0200

    reward functions tests

--- experiments/mnistdrawing1_0/test_environnment.py
+++ experiments/mnistdrawing1_0/test_environnment.py
@@ -16,7 +16,7 @@ def test_calc_dir_length():
         env._calc_dir_length(0)
         env._calc_dir_length(201)
 
-def test_calc_dir_length_nodraw():
+''' def test_calc_dir_length_nodraw():
     env = Canvas(np.zeros((28, 28)))
     
     assert env._calc_dir_length(-1) == (1, 1, False)
@@ -27,4 +27,16 @@ def test_calc_dir_length_nodraw():
     
     with pytest.raises(ValueError):
         env._calc_dir_length(0)
-        env._calc_dir_length(-201)
+        env._calc_dir_length(-201) '''
+        
+def test_reward_function():
+    env = Canvas(np.zeros((28, 28)))
+    ori = env.originalImage.reshape(28, 28)
+    ori[5, 6] = 255
+    env.originalImage = ori.reshape(28*28,)
+    
+    time_step = env._step(4)
+    
+    assert time_step.reward.max() == 1.0
+    time_step = env._step(4)
+    assert time_step.reward.max() == 0.0
