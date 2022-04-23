14c3b4bc8be8d0c96aeb0fdd62f2d4cc7f0ad918
RobRob1101 <robin.steiner00@gmail.com>
2022-04-23 18:19:28 +0200

    many changes many fixes

--- paper rebuild tf2/environment.py
+++ paper rebuild tf2/environment.py
@@ -1,7 +1,9 @@
 import os
 import random
+from types import NoneType
 import numpy as np
 import math as ma
+import time
 
 class ShapeDraw(object):
     def __init__(self, sidelength, patchsize, referenceData):
@@ -27,7 +29,7 @@ class ShapeDraw(object):
         self.curRef = 0
         self.agentPos = [0,0]
         self.isDrawing = 0 # 0 = not Drawing, 1 = Drawing (not bool because NN)
-        self.set_agentPos([random.randrange(0, self.s), random.randrange(0, self.s)])
+        self.set_agentPos([random.randint(1, self.s-1), random.randrange(1, self.s-1)])
 
     def step(self, agent_action):
         action = [0,0]
@@ -39,18 +41,31 @@ class ShapeDraw(object):
             y -= self.p
             self.isDrawing = 0
         
-        action = [x,y]
+        ownpos = (self.p-1)/2
+        action = [int(self.agentPos[0]+x-ownpos) , int(self.agentPos[1]+y-ownpos)]
+        
+        print(self.agentPos)
+        print(action)
+
+
+        penalty = False
+        if abs(x) < (self.p-1)/2 or abs(y) < (self.p-1)/2:
+            penalty = True
+
+
         if self.move_isLegal(action):
             self.set_agentPos(action)
         else:
             self.isDrawing = False
+            penalty = True
         
-        reward = self.reward() if self.isDrawing else 0
+        reward = self.reward(penalty) if self.isDrawing else 0
 
         return np.array([self.reference, self.canvas, self.distmap, self.colmap]), np.array([self.ref_patch, self.canvas_patch]), reward
 
     def set_agentPos(self, pos):
         if self.isDrawing:
+            #print(type(self.canvas), type(drawline(self.agentPos, pos, self.canvas)))
             self.canvas = drawline(self.agentPos, pos, self.canvas)
         self.agentPos = pos
         self.update_distmap()
@@ -75,27 +90,33 @@ class ShapeDraw(object):
         patchY = int(self.agentPos[1]-(self.p-1)/2)
         for y in range(self.p):
             for x in range(self.p):
-                yInd = 0 if patchY+y >= len(self.reference) else patchY+y
-                xInd = 0 if patchX+x >= len(self.reference[0]) else patchX+x
+                yInd = 0 if patchY+y >= len(self.reference) or patchY+y < 0 else patchY+y
+                xInd = 0 if patchX+x >= len(self.reference[0]) or patchX+x < 0 else patchX+x
+
                 self.ref_patch[y][x] = self.reference[yInd][xInd]
                 self.canvas_patch[y][x] = self.canvas[yInd][xInd]
 
-    def reward(self):
+    def reward(self, penalty):
+        #calculates reward of action based on gained similarity and length of step
         reward = 0
         similarity = 0
         for i in range(self.s):
             for j in range(self.s):
                 similarity += (self.canvas[i][j] - self.reference[i][j])**2
         similarity = similarity/(self.s**2)
-        reward = similarity - self.lastSim
+        
+        #Penalize steps smaller than maximum (reward speed)
+        small_step_penalty = 0
+        if penalty: small_step_penalty = -0.0005
+        reward = self.lastSim - similarity + small_step_penalty
         self.lastSim = similarity
 
-        return reward # Add on (from paper): Reward big steps
+        return reward
 
     def move_isLegal(self, action):
-        if action[0] >= len(self.canvas[0]) or action[0] < 0:
+        if action[0] >= len(self.canvas[0])-1 or action[0] < 1:
             return False
-        if action[1] >= len(self.canvas) or action[1] < 0:
+        if action[1] >= len(self.canvas)-1 or action[1] < 1:
             return False
         return True
 
@@ -103,7 +124,8 @@ class ShapeDraw(object):
         self.curRef += 1
         self.reference = self.referenceData[self.curRef]
         self.canvas = np.full((self.s, self.s), 1)
-        self.set_agentPos((random.randrange(0, self.s), random.randrange(0, self.s)))
+        self.set_agentPos((random.randint(0, self.s), random.randint(1, self.s-1)))
+        self.reward(False)
         return np.array([self.reference, self.canvas, self.distmap, self.colmap]), np.array([self.ref_patch, self.canvas_patch])
     
 
@@ -115,7 +137,7 @@ def drawline(setpos, pos, canvas):
     linePix = []
 
     if dx == 0 and dy == 0:
-        return
+        return canvas
 
     if abs(dx) > abs(dy):
         inc = int(ma.copysign(1,dx))
@@ -134,8 +156,8 @@ def drawline(setpos, pos, canvas):
             linePix[i][1] = setpos[1]+move
 
         for pix in linePix:
-            canvas[pix[1]][pix[0]-weight] = 1
-            canvas[pix[1]][pix[0]+weight] = 1
+            canvas[pix[1]-weight][pix[0]] = 1
+            canvas[pix[1]+weight][pix[0]] = 1
             canvas[pix[1]][pix[0]] = 1
     else:
         inc = int(ma.copysign(1,dy))
@@ -157,5 +179,5 @@ def drawline(setpos, pos, canvas):
             canvas[pix[1]][pix[0]+weight] = 1
             canvas[pix[1]][pix[0]-weight] = 1
             canvas[pix[1]][pix[0]] = 1
-    
+
     return canvas
