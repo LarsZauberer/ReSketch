8712a3692adc648a8344126335e3905c06f27760
Robin <robin.steiner00@gmail.comm>
2022-04-24 12:36:07 +0200

    optimization working

---
+++ paper rebuild tf2/environment test.py
@@ -0,0 +1,28 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+from environment import ShapeDraw, drawline
+
+ref_Data = pd.read_csv("C:/Users/robin/OneDrive/Desktop/Maturarbeit/Nachzeichner-KI/paper rebuild/ref_Data.csv")
+ref_Data = ref_Data.drop('Unnamed: 0', axis=1)
+train_ind = np.random.choice(ref_Data.shape[1], 3000)
+reference = []
+for i in train_ind:
+    reference.append(ref_Data.iloc[i].to_numpy().reshape(28,28))
+
+
+
+env = ShapeDraw(28,7,reference)
+plt.figure(figsize=(7,7))
+plt.imshow(env.canvas, interpolation='none', cmap='gray')
+
+#print(env.canvas_patch)
+
+g, l, rew = env.step(30)
+
+print(rew)
+#print(l[1])
+
+plt.imshow(env.canvas, interpolation='none', cmap='gray')
+plt.show()
--- paper rebuild tf2/main.py
+++ paper rebuild tf2/main.py
@@ -9,32 +9,29 @@ import random
 if __name__ == '__main__': 
     load_checkpoint = False
 
-    ref_Data = pd.read_csv("C:/Users/robin/OneDrive/Desktop/Maturarbeit/Nachzeichner-KI/paper rebuild/ref_Data.csv")
-    ref_Data = ref_Data.drop('Unnamed: 0', axis=1)
-    train_ind = np.random.choice(ref_Data.shape[1], 3000)
-    reference = []
-    for i in train_ind:
-        reference.append(ref_Data.iloc[i].to_numpy().reshape(28,28))
-   
-
+    #parameters
     canvas_size = 28
     patch_size = 7
     n_actions = 2*(patch_size**2)
-    mem_size = 3000
+    mem_size = 10000
     batch_size = 64
+    num_episodes = 6000
+    num_steps = 50
 
-  
+    #loading reference Data
+    ref_Data = pd.read_csv("paper rebuild tf2/ref_Data.csv")
+    ref_Data = ref_Data.drop('Unnamed: 0', axis=1)
+    train_ind = np.random.choice(ref_Data.shape[1], num_episodes)
+    reference = []
+    for i in train_ind:
+        reference.append(ref_Data.iloc[i].to_numpy().reshape(28,28))
+
+    #Initializing architecture
     env = ShapeDraw(canvas_size, patch_size, reference)
     agent = Agent(gamma=0.99, epsilon=0.5, alpha=0.001, global_input_dims=(4, canvas_size, canvas_size), 
                     local_input_dims=(2, patch_size, patch_size), mem_size=mem_size, batch_size=batch_size)
     if load_checkpoint:
         agent.load_models()
-    scores = []
-    eps_history = []
-    num_episodes = 6000
-    num_steps = 50
-    
-    score = 0
     
     #Fill replay buffer
     g_obs, l_obs = env.reset()
@@ -45,7 +42,11 @@ if __name__ == '__main__':
             g_obs = next_g_obs
             l_obs = next_l_obs
     
-
+    plt.figure(figsize=(7,7))
+    score = 0
+    scores = []
+    eps_history = []
+    #Main process
     for i in range(num_episodes):
         global_obs, local_obs = env.reset()
         score = 0
@@ -63,14 +64,14 @@ if __name__ == '__main__':
             if j % 4 == 0:
                 agent.learn()
 
-            
-    
-        # Learn Process visualization
+        # Learn Process visualization  
         if i % 12 == 0 and i > 0:
             avg_score = np.mean(scores[max(0, i-12):(i+1)])
             print('episode: ', i,'score: ', score,
                  ' average score %.3f' % avg_score,
                 'epsilon %.3f' % agent.epsilon)
+            plt.imshow(env.canvas, interpolation='none', cmap='gray')
+            plt.pause(0.01)
             agent.save_models()
         else:
             print('episode: ', i,'score: ', score)
