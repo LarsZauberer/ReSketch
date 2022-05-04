fd49411d6fbe02839e3989e0653bcd21860d0536
LarsZauberer <wasser.ian@gmail.com>
2022-05-04 20:50:25 +0200

    autopep8 and adding comments to step function

--- src/agent_modules/environment.py
+++ src/agent_modules/environment.py
@@ -6,104 +6,120 @@ import matplotlib.pyplot as plt
 
 
 class ShapeDraw(object):
-    def __init__(self, sidelength, patchsize, referenceData):
+    def __init__(self, sidelength: int, patchsize: int, referenceData: np.array):
         self.s = sidelength
-        self.p = patchsize #sidelength of patch (local Input). must be odd
-        
-        #Input gloabal stream
+        self.p = patchsize  # sidelength of patch (local Input). must be odd
+
+        # Input gloabal stream
         self.referenceData = referenceData
         self.canvas = np.zeros((self.s, self.s))
         self.distmap = np.zeros((self.s, self.s))
         self.colmap = np.zeros((self.s, self.s))
-        
-        #Input local stream
+
+        # Input local stream
         self.ref_patch = np.zeros((self.p, self.p))
         self.canvas_patch = np.zeros((self.p, self.p))
 
-        #possible outputs
-        self.n_actions= self.p*self.p*2 # For each pixel, is an action option (location of that pixel)
+        # possible outputs
+        # For each pixel, is an action option (location of that pixel)
+        self.n_actions = self.p*self.p*2
 
-        #initializes rest
-        self.lastSim = 0 # Last similarity between reference and canvas
-        self.reference = referenceData[0] # Pick just the first image of the reference data in the first initialization
+        # initializes rest
+        self.lastSim = 0  # Last similarity between reference and canvas
+        # Pick just the first image of the reference data in the first initialization
+        self.reference = referenceData[0]
         self.curRef = 0
-        self.agentPos = [0,0]
-        self.isDrawing = 0 #! 0 = not Drawing, 1 = Drawing (not bool because NN)
-        self.set_agentPos([random.randint(1, self.s-2), random.randrange(1, self.s-2)]) # Set a random start location for the agent (but with one pixel margin)
+        self.agentPos = [0, 0]
+        # ! 0 = not Drawing, 1 = Drawing (not bool because NN)
+        self.isDrawing = 0
+        # Set a random start location for the agent (but with one pixel margin)
+        self.set_agentPos([random.randint(1, self.s-2),
+                          random.randrange(1, self.s-2)])
 
-        #rendering / visualization
+        # rendering / visualization
         self.fig = plt.figure(figsize=(10, 7))
 
-    def step(self, agent_action):
+    def step(self, agent_action: int):
         action = [0, 0]
         self.isDrawing = 1
-        
+
+        # Calculate the x and y position coordinates of action in the current patch
         x = agent_action % self.p
         y = agent_action // self.p
         if y >= self.p:
             y -= self.p
             self.isDrawing = 0
-        
-        ownpos = (self.p-1)/2
-        action = [int(self.agentPos[0]+x-ownpos) , int(self.agentPos[1]+y-ownpos)]
 
+        # Calculate the global aim location of the action
+        ownpos = (self.p-1)/2
+        action = [int(self.agentPos[0]+x-ownpos),
+                  int(self.agentPos[1]+y-ownpos)]
+
+        # ? What does this code do?
         penalty = 0
         if abs(x) < ownpos or abs(y) < ownpos:
             penalty = -0.0005
 
+        # Draw if the move is legal
         if self.move_isLegal(action):
             self.set_agentPos(action)
         else:
+            # Give a penalty for an illegal move
             self.isDrawing = 0
             penalty = -0.001
-        
+
+        # Calculate the reward for the action in this turn
+        # The reward can be 0 because it is gaining the reward only for new pixels
         reward = self.reward() if self.isDrawing else 0.0
         reward += penalty
 
+        # Ending the timestep
         return np.array([self.reference, self.canvas, self.distmap, self.colmap]), np.array([self.ref_patch, self.canvas_patch]), reward
 
     def set_agentPos(self, pos):
         if self.isDrawing:
-            #print(type(self.canvas), type(drawline(self.agentPos, pos, self.canvas)))
+            # print(type(self.canvas), type(drawline(self.agentPos, pos, self.canvas)))
             self.canvas = drawline(self.agentPos, pos, self.canvas)
         self.agentPos = pos
         self.update_distmap()
         self.update_patch()
         self.update_colmap()
-        
+
     def update_distmap(self):
         x0 = self.agentPos[0]
         y0 = self.agentPos[1]
         for y in range(self.s):
             for x in range(self.s):
-                dist =  ma.sqrt((x-x0)**2 + (y-y0)**2)/self.s
+                dist = ma.sqrt((x-x0)**2 + (y-y0)**2)/self.s
                 self.distmap[y][x] = dist
 
     def update_colmap(self):
         for y in range(self.s):
             for x in range(self.s):
                 self.colmap[y][x] = self.isDrawing
-    
+
     def update_patch(self):
         patchX = int(self.agentPos[0]-(self.p-1)/2)
         patchY = int(self.agentPos[1]-(self.p-1)/2)
         for y in range(self.p):
             for x in range(self.p):
-                yInd = 0 if patchY+y >= len(self.reference) or patchY+y < 0 else patchY+y
-                xInd = 0 if patchX+x >= len(self.reference[0]) or patchX+x < 0 else patchX+x
+                yInd = 0 if patchY + \
+                    y >= len(self.reference) or patchY+y < 0 else patchY+y
+                xInd = 0 if patchX + \
+                    x >= len(self.reference[0]) or patchX+x < 0 else patchX+x
 
                 self.ref_patch[y][x] = self.reference[yInd][xInd]
                 self.canvas_patch[y][x] = self.canvas[yInd][xInd]
 
     def reward(self):
-        #calculates reward of action based on gained similarity and length of step
+        # calculates reward of action based on gained similarity and length of step
         reward = 0
         similarity = 0
         for i in range(self.s):
             for j in range(self.s):
                 similarity += (self.canvas[i][j] - self.reference[i][j])**2
         similarity = similarity/(self.s**2)
-        
+
         reward = self.lastSim - similarity
         self.lastSim = similarity
 
@@ -120,14 +136,13 @@ class ShapeDraw(object):
         self.curRef += 1
         self.reference = self.referenceData[self.curRef]
         self.canvas = np.zeros((self.s, self.s))
-        self.set_agentPos((random.randint(1, self.s-2), random.randint(1, self.s-2)))
+        self.set_agentPos((random.randint(1, self.s-2),
+                          random.randint(1, self.s-2)))
         self.reward()
         return np.array([self.reference, self.canvas, self.distmap, self.colmap]), np.array([self.ref_patch, self.canvas_patch])
-    
+
     def render(self, mode="None", realtime=False):
-        
-            
-        
+
         if mode == "Compare":
             rendCanv = self.canvas.copy().reshape(self.s**2,)
             rendRef = self.reference.copy().reshape(self.s**2,)
@@ -140,49 +155,48 @@ class ShapeDraw(object):
                     rendCanv[index] = 50
                 elif e == 1:
                     rendRef[index] = 50
-            
-            
 
-            
             # Original image
             self.fig.add_subplot(2, 2, 1)
-            plt.imshow(rendRef.reshape(28, 28), cmap='gray', label='Original', vmin=0, vmax=255)
+            plt.imshow(rendRef.reshape(28, 28), cmap='gray',
+                       label='Original', vmin=0, vmax=255)
             plt.axis("off")
             plt.title("Original")
-            
+
             rendCanv = rendCanv.reshape(28, 28)
 
             if realtime:
                 rendCanv[self.agentPos[1]][self.agentPos[0]] = 150
             # AI Generated Image
-            self.fig.add_subplot(2, 2, 2) 
-            plt.imshow(rendCanv, cmap='gray', label='AI Canvas', vmin=0, vmax=255)
+            self.fig.add_subplot(2, 2, 2)
+            plt.imshow(rendCanv, cmap='gray',
+                       label='AI Canvas', vmin=0, vmax=255)
             plt.axis("off")
             plt.title("AI Canvas")
-            
+
             plt.pause(0.01)
         else:
             plt.imshow(self.canvas, interpolation='none', cmap='gray')
             plt.pause(0.01)
-    
 
-#draws Line directly on bitmap to save convert
+
+# draws Line directly on bitmap to save convert
 def drawline(setpos, pos, canvas):
     weight = 1
-    dx = pos[0] - setpos[0] 
-    dy = pos[1] - setpos[1] 
+    dx = pos[0] - setpos[0]
+    dy = pos[1] - setpos[1]
     linePix = []
 
     if dx == 0 and dy == 0:
         return canvas
 
     if abs(dx) > abs(dy):
-        inc = int(ma.copysign(1,dx))
+        inc = int(ma.copysign(1, dx))
         for i in range(0, dx+inc, inc):
             linePix.append([setpos[0]+i, 0])
-        
+
         step = dy/(abs(dx)+1)
-        sign = int(ma.copysign(1,dy))
+        sign = int(ma.copysign(1, dy))
         move = 0
         res = 0
         for i in range((abs(dx)+1)):
@@ -197,12 +211,12 @@ def drawline(setpos, pos, canvas):
             canvas[pix[1]+weight][pix[0]] = 1
             canvas[pix[1]][pix[0]] = 1
     else:
-        inc = int(ma.copysign(1,dy))
+        inc = int(ma.copysign(1, dy))
         for i in range(0, dy+inc, inc):
             linePix.append([0, setpos[1]+i])
-        
+
         step = dx/(abs(dy)+1)
-        sign = int(ma.copysign(1,dx))
+        sign = int(ma.copysign(1, dx))
         move = 0
         res = 0
         for i in range((abs(dy)+1)):
