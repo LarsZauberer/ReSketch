073fe4ca568511acbce3cf901438ad404456598d
Robin <robin.steiner00@gmail.comm>
2022-07-15 10:34:08 +0200

    test script

--- src/data/data_prep.py
+++ src/data/data_prep.py
@@ -3,37 +3,6 @@ import json
 import numpy as np
 
 
-def generate_json():
-    label_Data = pd.read_csv("src/data/labels.csv")
-    ref_Data = pd.read_csv("src/data/ref_Data.csv")
-    ref_Data = ref_Data.drop('Unnamed: 0', axis=1)
-    label_Data = label_Data.drop('Unnamed: 0', axis=1)
-
-    data = [[] for _ in range(10)]
-
-
-    ref_Data["label"] = label_Data["label"]
-
-
-
-    for i in range(10):
-        num = ref_Data.loc[ref_Data["label"] == i]
-        num = num.drop("label", axis=1)
-    
-        lis = []
-        for j in  range(num.shape[0]):
-            lis.append( num.iloc[j].tolist() )
-        
-        
-        data[i] = lis
-
-    print(data[0][0])
-
-    with open("src/data/ref_Data.json", "w") as f:
-        json.dump(data, f)
-
-
-
 def sample_data(data, number):
     """  """
     sampled = []
@@ -55,6 +24,45 @@ def shuffle_data(data: list):
         d.append(data[i])
 
     return d
+
+
+
+
+def generate_json():
+    label_Data = pd.read_csv("src/data/labels.csv")
+    ref_Data = pd.read_csv("src/data/ref_Data.csv")
+    label_Data = label_Data.drop('Unnamed: 0', axis=1)
+    ref_Data = ref_Data.drop('Unnamed: 0', axis=1)
+    
+
+    data = [[] for _ in range(10)]
+
+
+    ref_Data["label"] = label_Data["label"]
+
+    ref_Data = ref_Data.tail(6000)
+
+    
+
+    for i in range(10):
+        num = ref_Data.loc[ref_Data["label"] == i]
+        num = num.drop("label", axis=1)
+    
+        lis = []
+        for j in  range(num.shape[0]):
+            lis.append( num.iloc[j].tolist() )
+        
+        
+        data[i] = lis
+
+    with open("src/data/test_ref_Data.json", "w") as f:
+        json.dump(data, f)
+
+generate_json()
+
+
+
+
 
         
 
