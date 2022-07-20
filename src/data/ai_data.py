ecf980719f005829a2ce326603f820a32f86a581
RobRob1101 <robin.steiner00@gmail.com>
2022-07-20 16:28:50 +0200

    docstrings

--- src/data/ai_data.py
+++ src/data/ai_data.py
@@ -17,7 +17,16 @@ class AI_Data():
        
         
     def sample(self, number: int):
-        """  """
+        """ 
+        Selects random instances in the reference data. 
+        Selects an equal amount of data from each category (each number)
+
+        :param number: the number of instances to be contained in the selection
+        :type number: int
+
+        :return: None. It updates the pro_data variable
+        :rtype: None
+        """
         sampled = []
         num = int(number/10)
         for n in range(10):
@@ -29,7 +38,12 @@ class AI_Data():
 
         
     def shuffle(self):
-        """ """
+        """ 
+        changes the order in the sampled data randomly
+
+        :return: None. It updates the pro_data variable
+        :rtype: None
+        """
         ind = np.arange(len(self.pro_data))
         np.random.shuffle(ind)
         shuffled = []
@@ -41,6 +55,13 @@ class AI_Data():
 
 
     def generate_json(self):
+        """
+        Creates a json file containing the given reference data ordered into
+        categories from 0 to 9
+
+        :return: None. Creates a json file
+        :rtype: None
+        """
         label_Data = pd.read_csv("src/data/labels.csv")
         ref_Data = pd.read_csv("src/data/ref_Data.csv")
         label_Data = label_Data.drop('Unnamed: 0', axis=1)
