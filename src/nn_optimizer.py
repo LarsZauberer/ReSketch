ae24473052d0649e85d1780f9c9731d0813d93fc
LarsZauberer <wasser.ian@gmail.com>
2022-08-30 21:21:35 +0200

    nn_optimizer finished
    added some more arguments to the training function in `nn_train.py`
    added an baysian optimization script to the project

---
+++ src/nn_optimizer.py
@@ -0,0 +1,138 @@
+from nn_test import Test_NN
+from nn_train import train
+from result_stats.learn_plotter import Learn_Plotter
+from bayes_opt import BayesianOptimization
+from agent_modules.nn_agent import DeepQNetwork, Agent
+from agent_modules.environment import ShapeDraw
+from data.ai_data import AI_Data
+
+
+def create_runner(args):
+    def runner(gamma, epsilon, alpha, replace_target, episode_mem_size):
+        log = logging.getLogger("optimizer-runner")
+        
+        # Fix parameters
+        canvas_size = 28
+        patch_size = 7
+        n_actions = 2*(patch_size**2)
+        episode_mem_size = int(episode_mem_size)
+        batch_size = 64
+        n_episodes = args.t
+        n_steps = 64
+        n_epochs = 1
+        
+        # further calculations
+        glob_in_dims = (4, canvas_size, canvas_size)
+        loc_in_dims = (2, patch_size, patch_size)
+        mem_size = episode_mem_size*n_steps
+        
+        # load training data
+        learn_plot = Learn_Plotter(path="src/result_stats/plotlearn_data.json")
+        data = AI_Data(dataset="mnist")
+        data.sample(n_episodes)
+        log.debug(f"Loaded Training data")
+        
+        # Creating environment and agent
+        env = ShapeDraw(canvas_size, patch_size, data.pro_data)
+        agent_args = {"gamma": gamma, "epsilon": epsilon, "alpha": alpha, "replace_target": replace_target, 
+                    "global_input_dims": glob_in_dims, "local_input_dims": loc_in_dims, 
+                    "mem_size": mem_size, "batch_size": batch_size, 
+                    "q_next_dir": "src/nn_memory/q_next", "q_eval_dir": "src/nn_memory/q_eval"}
+        agent = Agent(**agent_args)
+        log.debug(f"Initiated Environmnet and Agent")
+        log.info(f"Training with parameters: gamma: {gamma}, epsilon: {epsilon}, alpha: {alpha}, replace_target: {replace_target}")
+        
+        # Training
+        log.debug(f"Start training")
+        train(env=env, agent=agent, data=data, learn_plot=learn_plot, n_episodes=n_episodes, n_steps=n_steps, n_epochs=n_epochs, n_actions=n_actions, episode_mem_size=episode_mem_size, save_training=False)
+        log.info(f"Training finished")
+        
+        log.debug(f"Run Tests")
+        tester = Test_NN(n_test=260, num_steps=n_steps, dataset=args.dataset)
+        reward, accuracy, datarec, speed = tester.test(agent=agent, t_reward=True, t_accuracy=True, t_datarec=True, t_speed=True, t_vis=False)
+        
+        scores = {"reward": reward, "accuracy": accuracy, "datarec": datarec, "speed": speed}
+        log.debug(f"Tests finished. Scores: {scores}")
+        
+        log.info(f"Testing score on criteria: {args.criteria} - {scores[args.criteria]}")
+        
+        return scores[args.criteria]
+    
+    return runner
+
+
+def main(args):
+    # Hyperparameters to optimize
+    bounds = {"gamma": (0.1, 1), "epsilon": (0, 1), "alpha": (
+        0.00001, 0.001), "replace_target": (1000, 10000), "episode_mem_size": (100, 2000)}
+
+    runner = create_runner(args)
+
+    optimizer = BayesianOptimization(
+        f=runner,
+        pbounds=bounds,
+        random_state=1,
+    )
+    
+    optimizer.maximize(init_points=args.r, n_iter=args.i)
+    
+    log.info(f"Best found parameters: {optimizer.max}")
+
+
+if __name__ == '__main__':
+    import logging
+    import sys
+    import os
+    import argparse
+    from pathlib import Path
+    import time
+    try:
+        from rich.logging import RichHandler
+    except ModuleNotFoundError:
+        logging.warning(
+            "Rich is not installed. Please install rich by typing: pip install rich")
+        sys.exit(1)
+
+    # Initiate argparsing
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-f", help="Specify your excel spreadsheed", action="store")
+    parser.add_argument("-v", help="Verbose",
+                        action="store_true", default=False)
+    parser.add_argument("-r", help="Number of random iterations", action="store", type=int, default=3)
+    parser.add_argument("-i", help="Number of iterations", action="store", type=int, default=5)
+    parser.add_argument("-d", "--dataset", help="Name of the dataset to run the test on", action="store", type=str, default="mnist")
+    parser.add_argument("-c", "--criteria", help="Criteria to improve the test on", action="store", type=str, default="accuracy")
+    parser.add_argument("-t", help="Number of episodes in training", action="store", type=int, default=1000)
+
+    args = parser.parse_args()
+    
+    # Check if logs folder exists
+    if not Path("logs").exists():
+        os.mkdir("logs")
+
+    # File Log handler
+    FileHandler = logging.FileHandler(Path("logs/log.log"))
+    logFormatter = logging.Formatter("%(asctime)s [%(threadName)-12.12s] [%(name)s] [%(levelname)-5.5s] %(message)s")
+    FileHandler.setFormatter(logFormatter)
+
+    # Initiate logger
+    if args.v:
+        FORMAT = "%(message)s"
+        logging.basicConfig(
+            level="DEBUG", format=FORMAT, datefmt="[%X]", handlers=[RichHandler(), FileHandler]
+        )
+    else:
+        FORMAT = "%(message)s"
+        logging.basicConfig(
+            level="INFO", format=FORMAT, datefmt="[%X]", handlers=[RichHandler(), FileHandler]
+        )
+
+    log = logging.getLogger()
+    log.debug(f"Logging initialized!")
+    starttime = time.time()
+
+    # Main Stuff
+    main(args)
+
+    log.debug(f"Application closed at {time.time() - starttime} seconds")
