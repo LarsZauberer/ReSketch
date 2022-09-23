e7263a204bdfeddcadd0bba3f73f0d187e4b30e5
RobRob1101 <robin.steiner00@gmail.com>
2022-09-23 11:44:30 +0200

    pretrained model loader

--- src_physics/nn_train.py
+++ src_physics/nn_train.py
@@ -100,12 +100,16 @@ if __name__ == '__main__':
         hyp_data = json.load(f)
         
     if mnist and speed:
+        model ="mnist-speed"
         hyp_data = hyp_data["mnist_speed"]
     elif mnist:
+        model = "mnist"
         hyp_data = hyp_data["mnist"]
     elif speed:
+        model = "speed"
         hyp_data = hyp_data["speed"]
     else:
+        model = "base"
         hyp_data = hyp_data["base"]
     
     print(f"Hyperparameters: {hyp_data}")
@@ -139,8 +143,7 @@ if __name__ == '__main__':
     env = ShapeDraw(canvas_size, patch_size, data.pro_data, n_actions=n_actions, max_action_strength=max_action_strength, friction=hyp_data["friction"], vel_1=hyp_data["vel_1"], vel_2=hyp_data["vel_2"])
     agent_args = {"gamma": hyp_data["gamma"], "epsilon": hyp_data["epsilon"], "alpha": hyp_data["alpha"], "replace_target": int(hyp_data["replace_target"]), 
                   "global_input_dims": glob_in_dims, "local_input_dims": loc_in_dims, 
-                  "mem_size": mem_size, "batch_size": batch_size, 
-                  "q_next_dir": "src/nn_memory/q_next", "q_eval_dir": "src/nn_memory/q_eval", "n_actions": n_actions}
+                  "mem_size": mem_size, "batch_size": batch_size, "model": model, "n_actions": n_actions}
     agent = Agent(**agent_args)
     
     # Start training
