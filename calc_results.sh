66adf7c06cda7001ab5e7fc91a1507878b59d40e
LarsZauberer <wasser.ian@gmail.com>
2022-10-04 00:29:50 +0200

    All results retested

--- calc_results.sh
+++ calc_results.sh
@@ -7,3 +7,21 @@ python src_physics/nn_test.py -s -t 2000 --version base
 python src_physics/nn_test.py -s -t 2000 --version mnist
 python src_physics/nn_test.py -s -t 2000 --version speed
 python src_physics/nn_test.py -s -t 2000 --version mnist-speed
+
+python src/nn_test.py -s -t 2000 --version base -d emnist
+python src/nn_test.py -s -t 2000 --version mnist -d emnist
+python src/nn_test.py -s -t 2000 --version speed -d emnist
+python src/nn_test.py -s -t 2000 --version mnist-speed -d emnist
+python src_physics/nn_test.py -s -t 2000 --version base -d emnist
+python src_physics/nn_test.py -s -t 2000 --version mnist -d emnist
+python src_physics/nn_test.py -s -t 2000 --version speed -d emnist
+python src_physics/nn_test.py -s -t 2000 --version mnist-speed -d emnist
+
+python src/nn_test.py -s -t 2000 --version base -d quickdraw
+python src/nn_test.py -s -t 2000 --version mnist -d quickdraw
+python src/nn_test.py -s -t 2000 --version speed -d quickdraw
+python src/nn_test.py -s -t 2000 --version mnist-speed -d quickdraw
+python src_physics/nn_test.py -s -t 2000 --version base -d quickdraw
+python src_physics/nn_test.py -s -t 2000 --version mnist -d quickdraw
+python src_physics/nn_test.py -s -t 2000 --version speed -d quickdraw
+python src_physics/nn_test.py -s -t 2000 --version mnist-speed -d quickdraw
