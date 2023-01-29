50421bb189402fd94d4dc85eb134e64efb89acc9
RobRob1101 <robin.steiner00@gmail.com>
2023-01-29 17:22:12 +0100

    physics train restructuring

--- src_physics/models/mnist_model/utils/__init__.py
+++
@@ -1,3 +0,0 @@
-from models.mnist_model.utils.layers import *
-from models.mnist_model.utils.visualization import AffineVisualizer, plotImages, plotWrongImages, plotHistory
-from models.mnist_model.utils.dataset import Dataset
