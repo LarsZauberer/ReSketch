7744f08e379764f961a132f1af69506b94cbc8f6
RobRob1101 <robin.steiner00@gmail.com>
2023-01-30 14:57:58 +0100

    new emnist model progress

---
+++ src/models/emnist_2/prediction.py
@@ -0,0 +1,30 @@
+import os
+import numpy as np
+
+import tensorflow as tf
+from keras import Model, Sequential
+from keras.layers import Conv2D, Dense, Flatten, Input, concatenate, MaxPool2D
+from keras.optimizers import SGD, Adam
+from keras.callbacks import ReduceLROnPlateau, EarlyStopping
+
+from data.ai_data import AI_Data
+import matplotlib.pyplot as plt
+
+
+
+if __name__ == "__main__":
+    
+
+
+
+
+
+
+
+    """ fig, ax = plt.subplots()
+    ax.imshow(img[0], cmap="gray")
+    plt.show() """
+
+    
+
+
