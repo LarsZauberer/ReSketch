f276b41881dcdbda3a051bcab5b925b49d6b893c
LarsZauberer <wasser.ian@gmail.com>
2023-01-31 18:12:33 +0100

    extracted hyperparameter_loader

--- src/extras/train_functions.py
+++ src/extras/train_functions.py
@@ -3,31 +3,6 @@ import json
 import numpy as np
 from pathlib import Path
 
-def hyperparameter_loader(path: str, modelName: str):
-    """
-    hyperparameter_loader Load the hyperparameters from the file
-
-    :param path: the path to the hyperparameter file
-    :type path: str
-    :param modelName: The name of the model
-    :type modelName: str
-    :return: Dictionary of the different hyperparameters of the model
-    :rtype: dict
-    """
-    # Load Hyperparameter data
-    with open(Path(path), "r") as f:
-        hyp_data = json.load(f)
-    if hyp_data.get(modelName, False):
-        #General model names: "base", "mnist", "speed" "mnist-speed"
-        hyp_data = hyp_data[modelName]
-    else:
-        #No known Model: give default Values
-        hyp_data = {"gamma": 0.7, "epsilon": 0, "alpha": 0.0002, "replace_target": 6000, "episode_mem_size": 100, "n_episodes": 50, "friction": 0.3, "vel_1": 0.9, "vel_2": 1.5} 
-    
-    return hyp_data
-
-
-
 
 def train(env, agent, data, learn_plot, episode_mem_size, n_episodes, n_steps, model_path, save_training=True, vis_compare=12, mnist=False, speed=False):
     """
